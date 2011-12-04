jnorthrup/jmodelica2014

commit c727b37e78f3bfd0e83f142f4e9553dd1ba391cd

#1670: Add temporary directory, where the binary model and any extra shared libraries are, to system library path before loading model binary. This is to ensure that extra shared libraries are found. Path will be reset directly after model has been loaded.
git-svn-id: https://svn.jmodelica.org/trunk@2914 7ae51c65-13d5-4921-9d80-6511ddd1419c

Python/src/jmodelica/jmi.py
@@ -273,6 +273,15 @@ def _returns_ndarray(dll_func, dtype, shape, ndim=1, order=None):
     
     dll_func.restype = ct.POINTER(dtype)
     dll_func.errcheck = conv_function
+    
+def get_platform_libpath():
+    #Detect libray path depending on platform
+    if sys.platform == 'win32':
+        return 'PATH'
+    elif sys.platform == 'darwin':
+        return 'DYLD_LIBRARY_PATH'
+    else:
+        return 'LD_LIBRARY_PATH'
 
 ## This is an api comment.
 # @param libname Name of library.
@@ -303,9 +312,17 @@ def load_DLL(libname, path):
     See also http://docs.python.org/library/ct.html
     """
 
+    # Temporarily add the value of 'path' to system library path in case the dll 
+    # is dependent on other dlls. In that case they should be located in 'path'. 
+    libpath = get_platform_libpath()
+    oldpath = os.getenv(libpath)
+    newpath = path + ";" + oldpath
+    os.putenv(libpath, newpath)
     # Don't catch this exception since it hides the actual source
     # of the error.
     dll = Nct.load_library(libname, path)
+    # Set back to the old path
+    os.putenv(libpath, oldpath)
     return dll
 
 

commit 14c1e900b494e5dd6699fb2e5944109ba681ae16

Fixed a bug introduced on Mac systems in r2914, see #1670.
git-svn-id: https://svn.jmodelica.org/trunk@2920 7ae51c65-13d5-4921-9d80-6511ddd1419c

Python/src/jmodelica/jmi.py
@@ -278,8 +278,6 @@ def get_platform_libpath():
     #Detect libray path depending on platform
     if sys.platform == 'win32':
         return 'PATH'
-    elif sys.platform == 'darwin':
-        return 'DYLD_LIBRARY_PATH'
     else:
         return 'LD_LIBRARY_PATH'
 
@@ -316,7 +314,12 @@ def load_DLL(libname, path):
     # is dependent on other dlls. In that case they should be located in 'path'. 
     libpath = get_platform_libpath()
     oldpath = os.getenv(libpath)
-    newpath = path + ";" + oldpath
+    if oldpath == None:
+        oldpath = ''
+    if sys.platform == 'win32':
+        newpath = path + ";" + oldpath
+    else:
+        newpath = path + ":" + oldpath
     os.putenv(libpath, newpath)
     # Don't catch this exception since it hides the actual source
     # of the error.